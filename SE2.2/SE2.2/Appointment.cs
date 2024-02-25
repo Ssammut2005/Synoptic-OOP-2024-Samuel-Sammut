1cf4572445ab984f44803778164f3c9e6b9fe141
Ssammut2005 <156342569+Ssammut2005@users.noreply.github.com>
2024-02-25 18:17:14 +0100

    SE2.2

---
+++ SE2.2/SE2.2/Appointment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE2._2
+{
+
+        class Appointment
+        {
+            public static void main(string[] args) { }
+            public Appointment(DateTime date)
+            {
+                Date = date;
+                Details = new furtherAppointmentDetails();
+            }
+
+            public string Title { get; set; }
+            public DateTime Date { get; set; }
+            public int Id { get; set; }
+            public furtherAppointmentDetails Details { get; }
+
+            public void AddTag(Tag tag)
+            {
+                Tag.Add(tag);
+            }
+
+        }
+
+        public class furtherAppointmentDetails
+        {
+            public string Description { get; set; }
+            public string Location { get; set; }
+
+
+        }
+        //I added the class furtherAppointmentDetails to enable a composition relationship
+
+       
+
+        
+    }
+
+
