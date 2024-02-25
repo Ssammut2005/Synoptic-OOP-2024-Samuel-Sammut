284d81de22c8a1a4e1e72e20eb3c21714d920697
Ssammut2005 <156342569+Ssammut2005@users.noreply.github.com>
2024-02-25 18:02:23 +0100

    KU1.1-1.3

---
+++ KU1.1-1.3/SynopticPartTwo/Appointment.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System;
+
+public class Appointment
+{
+    private List<Appointment> appointments = new List<Appointment>();
+
+    public Appointment(string title, DateTime date)
+    {
+        Date = date;
+        Title = title;
+    }
+
+    public string Title { get; set; }
+    public DateTime Date { get; set; }
+
+    public int Id { get; set; }
+
+    public int TagFk { get; set; }
+
+    //Mehtod to add new Appintmens
+
+    public void addNewAppointment(Appointment a)
+    {
+        if (a.Date > DateTime.Now)
+        {
+            appointments.Add(a);
+        }
+        else
+        {
+            Console.WriteLine("You have enetered a date in the past and therefore the appointment could not be created");
+        }
+    }
+
+
+}
+
+public class Calendar
+{
+    private string user;
+    private string title;
+    private DateTime startDate;
+
+    public string User
+    {
+        get { return user; }
+        private set { user = value; }
+    }
+
+    public string Title
+    {
+        get { return title; }
+        private set { title = value; }
+    }
+
+    public DateTime StartDate
+    {
+        get { return startDate; }
+        private set
+        {
+            if (value > DateTime.Now)
+            {
+                throw new Exception("Invalid Date");
+            }
+            else
+            {
+                startDate = value;
+            }
+        }
+
+
+
+    }
+
+}
---
+++ KU1.1-1.3/SynopticPartTwo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynopticPartTwo
+{
+    public class Program:Calendar
+    {
+        static void Main(string[] args)
+        {
+            Calendar calendar= new Calendar();
+        }
+    }
+}
